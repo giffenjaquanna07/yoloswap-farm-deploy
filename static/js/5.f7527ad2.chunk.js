9a732d220cdee4bd13031142943035f07de58717
giffenjaquanna07 <giffenjaquanna07@gmail.com>
2021-11-03 00:01:28 +0800

    first

---
+++ static/js/5.f7527ad2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpindex-ui"]=this["webpackJsonpindex-ui"]||[]).push([[5],{1179:function(i,n){}}]);
+//# sourceMappingURL=5.f7527ad2.chunk.js.map
---
+++ static/js/6.dba72fcd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpindex-ui"]=this["webpackJsonpindex-ui"]||[]).push([[6],{1229:function(i,n){}}]);
+//# sourceMappingURL=6.dba72fcd.chunk.js.map
